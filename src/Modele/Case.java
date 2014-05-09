3003c92bfc6762da833df68c42ddc50868a43ac9
mathieu <mathieu@Tornado>
2014-05-09 16:49:32 +0200

    début tetris

--- src/Modele/Case.java
+++ src/Modele/Case.java
@@ -11,5 +11,37 @@ package Modele;
  * @author mathieu
  */
 public class Case {
+    int x;
+    int y;
+    int ident;
     
+    public Case (int x, int y, int ident){
+        this.x = x;
+        this.y = y;
+        this.ident = ident;
+    }
+    
+    public int getX(){
+        return this.x;
+    }
+    
+    public int getY(){
+        return this.y;
+    }
+    
+    public void setX(int x){
+        this.x = x;
+    }
+    
+    public void setY(int y){
+        this.y = y;
+    }
+    
+    public int getIdent(){
+        return this.ident;
+    }
+    
+    public void setIdent(int ident){
+        this.ident = ident;
+    }
 }
---
+++ src/Modele/Grille.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Modele;
+
+/**
+ *
+ * @author mathieu
+ */
+public class Grille {
+    
+    Case grille[][];
+    
+    public Grille(){
+        grille = null;
+    }
+    
+    public Case[][] getGrille(){
+        return grille;
+    }
+    
+    /*
+    public Case[][] ajouterPiece(Piece p){
+        
+    }
+    */
+}
